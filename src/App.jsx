1fb5bd9e4ee3bef978301aaea27130de582106b6
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-05-23 01:25:33 +0200

    adding new issue in runtime

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const issues = [
+const initialIssues = [
     {
         id: 1, status: 'New', owner: 'Ravan', effort: 5,
         created: new Date('2018-08-15'), due: undefined,
@@ -10,6 +10,10 @@ const issues = [
         due: new Date('2018-08-30'), title: 'Missingbottom border on panel'
     },
 ];
+
+const sampleIssue = {
+status: 'New', owner: 'Pieta', titile: 'Completion date should be optional',
+};
 
 class IssueFilter extends React.Component {
     render() {
@@ -37,8 +41,36 @@ class IssueRow extends React.Component {
 }
 
 class IssueTable extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            issues: []
+        };
+        setTimeout(() => {
+            this.createIssue(sampleIssue);
+        }, 2000);
+    }
+
+    loadData() {
+        setTimeout(() => {
+            this.setState({ issues: initialIssues });
+        }, 500);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    createIssue(issue) {
+        issue.id = this.state.issues.length + 1;
+        issue.created = new Date();
+        const newIssueList = this.state.issues.slice();
+        newIssueList.push(issue);
+        this.setState({issues: newIssueList});
+    }
+
     render() {
-        const issuesRow = issues.map(issue =>
+        const issuesRow = this.state.issues.map(issue =>
             <IssueRow key={issue.id} issue={issue} />);
 
         return (
