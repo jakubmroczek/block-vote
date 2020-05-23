a809febef2cca042a831b1f66eb698712defee9f
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-05-23 12:40:02 +0200

    implemented simple user error handling

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,33 @@ function jsonDateReviver(key, value) {
     return value;
 }
 
+async function graphQLFetch(query, variables = {}) {
+    try {
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ query, variables })
+        });
+
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors) {
+            const error = result.errors[0];
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`)
+            }
+        }
+
+        return result.data;
+    } catch (e) {
+        alert(`Error in sending data to server: ${e.message}`);
+    }
+}
+
 function IssueRow(props) {
     const issue = props.issue;
     return (
@@ -98,14 +125,11 @@ class IssueList extends React.Component {
               created effort due
             }
           }`;
-        const response = await fetch('/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query })
-        });
-        const body = await response.text();
-        const result = JSON.parse(body, jsonDateReviver);
-        this.setState({ issues: result.data.issueList });
+
+        const data = await graphQLFetch(query);
+        if (data) {
+            this.setState({ issues: data.issueList });
+        }
     }
 
     componentDidMount() {
@@ -118,14 +142,12 @@ class IssueList extends React.Component {
                 issueAdd(issue: $issue) {
                     id
                 }
-            }` 
-        
-        const response = await fetch('/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query, variables: { issue }})
-        });
-        this.loadData();
+            }`
+
+        const data = await graphQLFetch(query, { issue });
+        if (data) {
+            this.loadData();
+        }
     }
 
     render() {
