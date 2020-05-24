04cd25b3c02e1a8f3cc497950c7d29ef5361c382
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-05-25 01:44:07 +0200

    frontend refactorization

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,9 @@
+/* globals React */
+// eslint-disable-next-line react/prefer-stateless-function
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
