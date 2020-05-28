7198dfee6ea14397e15227d3dd5dea2719c60e59
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-05-28 11:44:11 +0200

    uncommented module.hot swap

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -10,7 +10,3 @@ import IssueList from './IssueList.jsx';
 const element = <IssueList />;
 ReactDOM.render(element,
   document.getElementById('contents'));
-
-if (module.hot) {
-  module.hot.accept();
-}
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -21,4 +21,5 @@ module.exports = {
       chunks: 'all',
     },
   },
+  devtool: 'source-map',
 };
