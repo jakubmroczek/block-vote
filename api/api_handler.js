aed36771aee36ab754b9d5f8346d0280cd29f6e6
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-06 14:04:33 +0200

    removed pro mern classes

--- api/api_handler.js
+++ api/api_handler.js
@@ -2,31 +2,19 @@ require('dotenv').config();
 const fs = require('fs');
 const { ApolloServer } = require('apollo-server-express');
 
-const GraphQLDate = require('./graphql_date.js');
-const about = require('./about.js');
-const issue = require('./issue.js');
-
 const candidate = require('./candidate.js');
 const participant = require('./participant.js');
 const election = require('./election.js');
-
 const mailService = require('./mail_service.js');
 
 const resolvers = {
   Query: {
-    about: about.getMessage,
-    issueList: issue.list,
-    issue: issue.get,
     candidateList: candidate.list,
     participantList: participant.list,
     electionTitle: election.get,
     sendRegisterPublicKeysMail: mailService.sendRegisterKeyMail,
   },
   Mutation: {
-    setAboutMessage: about.setMessage,
-    issueAdd: issue.add,
-    issueUpdate: issue.update,
-    issueDelete: issue.delete,
     addCandiate: candidate.add,
     updateCandidate: candidate.update,
     removeCandidate: candidate.remove,
@@ -35,7 +23,6 @@ const resolvers = {
     updateParticipant: participant.update,
     updateElectionTitle: election.update,
   },
-  GraphQLDate,
 };
 
 const server = new ApolloServer({
