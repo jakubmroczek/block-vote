6699b80fcbf9b99fbc819c2f83fb8a9538486a9b
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-07 22:41:23 +0200

    navbar sketch

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+// import {
+// Navbar, Nav, NavItem, OverlayTrigger,
+// NavDropdown, MenuItem, Tooltip,
+// } from 'react-bootstrap';
+import {
+  Navbar, Nav, NavDropdown, Form, FormControl, Button,
+} from 'react-bootstrap';
 import Contents from './Contents.jsx';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">BlockVote</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Button variant="outline-success">Log out</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default function Page() {
   return (
     <div>
+      <NavBar />
       <Contents />
     </div>
   );
