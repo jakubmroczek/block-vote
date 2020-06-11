b654a423115d487c9b9828a6af05658f75c97e30
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-11 12:47:19 +0200

    user can succesfully log out from the app

--- ui/src/Contents.jsx
+++ ui/src/Contents.jsx
@@ -7,19 +7,23 @@ import ElectionSetUpPanel from './ElectionSetUpPanel.jsx';
 import ElectionLobby from './ElectionLobby.jsx';
 import RegisterKeyView from './RegisterKeyView.jsx';
 import UserPanel from './UserPanel.jsx';
+import NavBar from './NavBar.jsx';
 
 const NotFound = () => <h1>Page Not Found</h1>;
 
-export default function Contents() {
+export default function Contents({ onUserChange }) {
   return (
-    <Switch>
-      <Route exact path="/" component={Welcome} />
-      <Route path="/vote" component={VotingdApp} />
-      <Route exact path="/panel" component={UserPanel} />
-      <Route path="/panel/edit/:electionID" component={ElectionSetUpPanel} />
-      <Route exact path="/panel/lobby" component={ElectionLobby} />
-      <Route exact path="/panel/key" component={RegisterKeyView} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <Route path="/" render={props => <NavBar {...props} onUserChange={onUserChange} /> } />
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/vote" component={VotingdApp} />
+        <Route exact path="/panel" component={UserPanel} />
+        <Route path="/panel/edit/:electionID" component={ElectionSetUpPanel} />
+        <Route exact path="/panel/lobby" component={ElectionLobby} />
+        <Route exact path="/panel/key" component={RegisterKeyView} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import {
-  Navbar, Nav, Button,
-} from 'react-bootstrap';
 import { Switch, Route } from 'react-router-dom';
 import UserContext from './UserContext.js';
 import Contents from './Contents.jsx';
@@ -14,76 +11,6 @@ function NotFound() {
   );
 }
 
-class LogOutButton extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      initialized: true,
-    };
-    this.signOut = this.signOut.bind(this);
-  }
-
-  async init() {
-    const clientId = window.ENV.GOOGLE_CLIENT_ID;
-    if (!clientId) {
-      return;
-    }
-    window.gapi.load('auth2', () => {
-      if (!window.gapi.auth2.getAuthInstance()) {
-        window.gapi.auth2.init({ client_id: clientId }).then(() => {
-          this.setState({ intialized: true });
-        });
-      }
-    });
-  }
-
-  async signOut() {
-    await this.init();
-
-    const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
-
-    try {
-      await fetch(`${apiEndpoint}/signout`, {
-        method: 'POST',
-      });
-      const auth2 = window.gapi.auth2.getAuthInstance();
-      await auth2.signOut();
-      // TODO: I must set user state heres
-      // this.setState({ user: { signedIn: false, usrename: ''}})
-
-      const { onLogout } = this.props;
-      onLogout();
-    } catch (error) {
-      alert(`Error signing out ${error}`);
-    }
-  }
-
-  render() {
-    return (
-      <Button variant="outline-success" onClick={this.signOut}>Log out</Button>
-    );
-  }
-}
-
-function NavBar({ user, onLogout }) {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">BlockVote</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Navbar.Text>
-          Signed in as:
-          {' '}
-          {/* {user.username} */}
-        </Navbar.Text>
-        <Nav className="mr-auto">
-          <LogOutButton onLogout={onLogout} />
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-}
-
 export default class Page extends React.Component {
   constructor() {
     super();
@@ -92,17 +19,10 @@ export default class Page extends React.Component {
     };
 
     this.onUserChange = this.onUserChange.bind(this);
-    this.onLogout = this.onLogout.bind(this);
   }
 
   onUserChange(user) {
     this.setState({ user });
-  }
-
-  onLogout() {
-    this.setState({ user: { signedIn: false, username: '' } });
-    const { history } = this.props;
-    history.push('/');
   }
 
   render() {
@@ -111,9 +31,8 @@ export default class Page extends React.Component {
     if (user.signedIn) {
       return (
         <>
-          <NavBar onLogout={this.onLogout} />
           <UserContext.Provider value={user}>
-            <Contents />
+            <Contents onUserChange={this.onUserChange} />
           </UserContext.Provider>
         </>
       );
