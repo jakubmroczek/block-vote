de9773f0fc6fdb4910e14b3030fe95a8cbae70aa
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-12 01:48:59 +0200

    algorithm for veryfing the send token

--- api/voter.js
+++ api/voter.js
@@ -1,32 +1,48 @@
 require('dotenv').config();
 const election = require('./election.js');
 
-function verifySecretToken(electionID, secretToken) {
+function secretTokenMatches(participant, secretToken) {
+  return participant.secretToken === secretToken;
+}
+
+function isRegistered(participant) {
+  return participant.isRegistered;
+}
+
+// TODO: Better name than electionDB
+function getParticipant(electionDB, secretToken) {
+  const { participants } = election;
+
+  const participant = participants.find(p => p.secretToken === secretToken);
+
+  // TODO: What if is undefined
+  return participant;
+}
+
+async function verifySecretToken(electionID, secretToken) {
   // Read election
+  const id = electionID;
+  const electionDB = await election.get({}, { id });
 
   // Find participant having the secretToken
+  const participant = getParticipant(electionDB, secretToken);
 
-  // Check if participant is registered 
-
-  // Check if the tokens matches
-
-  // if matches return true else false
-
-  return false;
+  // Check if participant is registered
+  return isRegistered(participant) && secretTokenMatches(participant, secretToken);
 }
 
 // TODO: Change name or move to the election.js
 function tryRegisterPublicKey(electionID, publicKey) {
-    // Read election
+  // Read election
 
-    // Find particpant 
+  // Find particpant
 
-    // Mark participatn as registered
+  // Mark participatn as registered
 
-    // Insert public key
+  // Insert public key
 
-    // return success
-    return false;
+  // return success
+  return false;
 }
 
 function registerPublicKey(_, { electionID, secretToken, publicKey }) {
