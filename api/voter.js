52bafe13a2d6396bf69a85637b2823e6a2c36bf3
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-12 01:36:38 +0200

    introduced extra parameter plus framework for adding the public key

--- api/voter.js
+++ api/voter.js
@@ -1,7 +1,37 @@
 require('dotenv').config();
+const election = require('./election.js');
 
-function registerPublicKey(_, { secretToken, publicKey }) {
-    return true;
+function verifySecretToken(electionID, secretToken) {
+  // Read election
+
+  // Find participant having the secretToken
+
+  // Check if participant is registered 
+
+  // Check if the tokens matches
+
+  // if matches return true else false
+
+  return false;
 }
 
-module.exports = { registerPublicKey }
+// TODO: Change name or move to the election.js
+function tryRegisterPublicKey(electionID, publicKey) {
+    // Read election
+
+    // Find particpant 
+
+    // Mark participatn as registered
+
+    // Insert public key
+
+    // return success
+    return false;
+}
+
+function registerPublicKey(_, { electionID, secretToken, publicKey }) {
+  return verifySecretToken(electionID, secretToken)
+            && tryRegisterPublicKey(electionID, publicKey);
+}
+
+module.exports = { registerPublicKey };
