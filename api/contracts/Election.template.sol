b4ba5e14bcad058cc0fe38098c8e70107aa94c3c
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-14 01:15:43 +0200

    fixed generating smart contract from the templat

--- api/contracts/Election.template.sol
+++ api/contracts/Election.template.sol
@@ -67,10 +67,10 @@ contract Election {
     m_electionTitle = electionTitle;
 
     // Substituted with data from database in the compilatin process.
-    %s
+%s
 
     // Substitute with data from database in the compilatin process.
-    %s
+%s
   }
 
   //TODO: Change to addCandidate
