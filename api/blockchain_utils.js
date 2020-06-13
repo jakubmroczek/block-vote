5fba8445407a782a0666bf97a8516fced3d1d137
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-13 17:40:54 +0200

    sample compilation added on the backend

--- api/blockchain_utils.js
+++ api/blockchain_utils.js
@@ -1,7 +1,38 @@
+const solc = require('solc');
+
 // Compiles the eleciton with the solc and returns the JSON bytecodes
 function compile(election) {
-    console.log(election);
-    return "dummy json";
+  // TODO: Format the election template
+  const input = {
+    language: 'Solidity',
+    sources: {
+      'test.sol': {
+        content: 'contract C { function f() public { } }',
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*'],
+        },
+      },
+    },
+  };
+
+  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+  // `output` here contains the JSON output as specified in the documentation
+  for (const contractName in output.contracts['test.sol']) {
+    console.log(
+      `${contractName
+      }: ${
+        output.contracts['test.sol'][contractName].evm.bytecode.object}`,
+    );
+  }
+
+  console.log(output);
+
+  return 'dummy json';
 }
 
-module.exports = { compile }
+module.exports = { compile };
