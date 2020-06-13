d7cd9ecd0e34bd1956a9d7711fb62cb3138f8dd6
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-13 12:24:56 +0200

    secret token input panel

--- ui/src/register/RegisterPublicKeyPanel.jsx
+++ ui/src/register/RegisterPublicKeyPanel.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+{ /* <Form.Group controlId="publicKey">
+<Form.Label>Public key</Form.Label>
+<Form.Control type="text" placeholder="Enter your Ethereum public keys" />
+<Form.Text className="text-muted">
+  Ethereum public key (look up the network name in the email).
+</Form.Text>
+</Form.Group>
+<Button variant="primary" type="submit">
+Submit
+</Button> */ }
 
 export default function RegisterPublicKeyPanel() {
-    return (
-        <>
-        <h1>Hello if you want the yo can register your public key here!</h1>
-        </>
-    )
+  const onSecretTokenSubmit = () => {
+    alert('secret token submitted');
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="secretToken">
+        <Form.Label>Secret token</Form.Label>
+        <Form.Control type="text" placeholder="Enter secret token" />
+        <Form.Text className="text-muted">
+          Secret token was send in the email.
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={onSecretTokenSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
