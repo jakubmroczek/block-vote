e0270f7252d4ec1f9cb78cbfa1e5bb863d30799d
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-13 10:45:10 +0200

    improved sending mail - there last only a link generator

--- api/mail_service.js
+++ api/mail_service.js
@@ -11,22 +11,30 @@ const transporter = nodemailer.createTransport({
 });
 
 // TODO: Take this from the UI server / graphql query parameter
-const htmlForm = '<p>Register your Ethereum public key here: <a href="http://localhost:8000/panel/key">Register your vote here mate</a></p>';
+const htmlForm = (link, secretToken) => `
+     <p>
+      Register your Ethereum public through this link
+      <a href="${link}">${link}</a>
+      Your very secret token is: ${secretToken} 
+      </p>
+`;
 
-const registerKeyMailTemplate = to => ({
+const mailTemplate = (to, html) => ({
   from: process.env.GMAIL_USER,
   to,
   subject: 'Register your public key',
-  html: htmlForm,
+  html,
 });
 
-// TODO: Each user should have a unique link?
-async function sendRegisterKeyMail(_, { id }) {
-  const electionDB = await election.get({}, { id });
-  const { participants } = electionDB;
-  const emails = participants.map(p => p.email).join();
+function generateMail(participant) {
+  const { email, secretToken } = participant;
+  // TODO: From where to we take the voting link?
+  const link = 'very-dumy-link';
+  const html = htmlForm(link, secretToken);
+  return mailTemplate(email, html);
+}
 
-  const mailOptions = registerKeyMailTemplate(emails);
+function sendEmail(mailOptions) {
   transporter.sendMail(mailOptions, (error /* info */) => {
     if (error) {
       // TOOD: Logging
@@ -35,9 +43,25 @@ async function sendRegisterKeyMail(_, { id }) {
     // TOOD: Logging
     return true;
   });
+}
+
+function mailEveryone(participants) {
+  const mails = participants.map(generateMail);
+
+  mails.forEach((mail) => {
+    sendEmail(mail);
+  });
 
   // TODO: Fix this, introducde better erorr handling
   return true;
+}
+
+// TODO: Each user should have a unique link?
+async function sendRegisterKeyMail(_, { id }) {
+  const electionDB = await election.get({}, { id });
+  const { participants } = electionDB;
+
+  return mailEveryone(participants);
 }
 
 module.exports = {
