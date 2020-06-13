4318de6448d8d0f5722330cbf4a7d3866dda3cf7
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-13 12:42:16 +0200

    readign data from the form

--- ui/src/register/RegisterPublicKeyPanel.jsx
+++ ui/src/register/RegisterPublicKeyPanel.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-{ /* <Form.Group controlId="publicKey">
-<Form.Label>Public key</Form.Label>
-<Form.Control type="text" placeholder="Enter your Ethereum public keys" />
-<Form.Text className="text-muted">
-  Ethereum public key (look up the network name in the email).
-</Form.Text>
-</Form.Group>
-<Button variant="primary" type="submit">
-Submit
-</Button> */ }
-
 export default function RegisterPublicKeyPanel() {
-  const onSecretTokenSubmit = () => {
-    alert('secret token submitted');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    
+    const form = document.forms.registerPublicKey;
+    const credentials = {
+      secretToken: form.secretToken.value,
+      publicKey: form.publicKey.value,
+    };
   };
 
   return (
-    <Form>
+    <Form name="registerPublicKey">
       <Form.Group controlId="secretToken">
         <Form.Label>Secret token</Form.Label>
         <Form.Control type="text" placeholder="Enter secret token" />
@@ -26,7 +21,14 @@ export default function RegisterPublicKeyPanel() {
           Secret token was send in the email.
         </Form.Text>
       </Form.Group>
-      <Button variant="primary" type="submit" onClick={onSecretTokenSubmit}>
+      <Form.Group controlId="publicKey">
+        <Form.Label>Public key</Form.Label>
+        <Form.Control type="text" placeholder="Enter your Ethereum public keys" />
+        <Form.Text className="text-muted">
+          Ethereum public key (look up the network name in the email).
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={onSubmit}>
         Submit
       </Button>
     </Form>
