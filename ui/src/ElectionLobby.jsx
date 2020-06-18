372b6f2e993b13da7f1a38f03622b937eef70be1
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-18 09:47:50 +0200

    fetching sm from backend in the front

--- ui/src/ElectionLobby.jsx
+++ ui/src/ElectionLobby.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import graphQLFetch from './graphQLFetch.js';
 
-export default function ElectionLobby({ id }) {
+export class  ElectionLobby extends React.Component {
+  
+  const fetchSmartContract = async () => {
+    const query = `query 
+      getElection($id: ID!) {
+              getElection(id: $id) {
+                  smartContract {
+                    serialziedValue
+                  }
+              }
+  }`;
+
+    const response = await graphQLFetch(query, { id });
+
+    if (response) {
+      const { smartContract } = response.getElection;
+      this.setState({ smartContract });
+    } else {
+      alert('getElection call failed');
+    }
+  };
+
+
   const deployElection = async () => {
     const query = `mutation 
     deployElection($id: ID!) {
@@ -15,12 +37,13 @@ export default function ElectionLobby({ id }) {
     const response = await graphQLFetch(query, { id });
 
     if (response) {
-      alert('Going to deploy the election on the blockchain')
+      alert('Going to deploy the election on the blockchain');
     } else {
-      alert('Could not deploy eleciton on the blockchain')
+      alert('Could not deploy eleciton on the blockchain');
     }
   };
 
+ render() {
   return (
     <>
       <h1>Please wait for voters to register</h1>
@@ -29,4 +52,5 @@ export default function ElectionLobby({ id }) {
       </Button>
     </>
   );
+ }
 }
