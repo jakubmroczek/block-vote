cbc86499952d0d79fd35e015a47570197ea02704
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-18 11:15:46 +0200

    fixed bug with ethereumjs-tx 2.0

--- ui/src/deploy.js
+++ ui/src/deploy.js
@@ -1,18 +1,19 @@
-const Tx = require('ethereumjs-tx');
+const Tx = require('ethereumjs-tx').Transaction;
 const Web3 = require('web3');
 
-const web3 = new Web3('https://ropsten.infura.io/v3/76ba732d07064c4ab4178385fc6c005f')
+const web3 = new Web3('https://ropsten.infura.io/v3/76ba732d07064c4ab4178385fc6c005f');
 
-const account1 ='0x63133471af7dbc3b4de87d071ef8d77b9c5912fc'
-const pK1 = '0xc39b1962b1910e8724095df3e3cb41150d6a6f1c0739a9869db1b195facee6c9'
+const account1 = '0x63133471af7dbc3b4de87d071ef8d77b9c5912fc';
+// const pK1 = Buffer.from('0xc39b1962b1910e8724095df3e3cb41150d6a6f1c0739a9869db1b195facee6c9');
+const pK1 = Buffer.from('c39b1962b1910e8724095df3e3cb41150d6a6f1c0739a9869db1b195facee6c9', 'hex');
 
-web3.eth.getTransactionCount(account1, (err, txCount) => {
-  // Smart contract data
+web3.eth.getTransactionCount(account1, (err, txCount) => {  
+    // Smart contract data
   // Fetch the compiled code from the backend
-  
+
   // This is some form of slightely chnaged bytecode
   const data = '0x608060405234801561001057600080fd5b5060c78061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80636057361d146037578063b05784b8146062575b600080fd5b606060048036036020811015604b57600080fd5b8101908080359060200190929190505050607e565b005b60686088565b6040518082815260200191505060405180910390f35b8060008190555050565b6000805490509056fea26469706673582212208dea039245bf78c381278382d7056eef5083f7d243d8958817ef447e0a403bd064736f6c63430006060033';
-  
+
   // Create transaction object
   const txObject = {
     nonce: web3.utils.toHex(txCount),
@@ -24,7 +25,7 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
   };
 
   // Sign the transaction
-  const tx = new Tx(txObject);
+  const tx = new Tx(txObject, {'chain':'ropsten'});
   tx.sign(pK1);
 
   const serializedTx = tx.serialize();
