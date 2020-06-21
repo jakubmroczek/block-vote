44d3205dea2a9f1c8ddcb7375c52eeca76abb77c
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-21 22:44:13 +0200

    change the datbase name use, .env updated also + console command

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.DB_URL || 'mongodb://localhost/blockvote';
   const client = new MongoClient(url, { newUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
