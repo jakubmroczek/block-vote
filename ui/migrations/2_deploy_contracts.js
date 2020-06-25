be34b734bec87ee408bb85ae4a55b1a4c9283c33
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-26 01:23:45 +0200

    removed truffle dirs

--- ui/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const Election = artifacts.require('Election');
-
-module.exports = function (deployer) {
-  const electionTitle = 'Election to the  student government';
-  deployer.deploy(Election, electionTitle);
-};
