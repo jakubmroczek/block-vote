fc0b04e613327e830fa2feee38a0539cd58588aa
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-06-28 13:23:02 +0200

    insalled mongoose

--- api/lib/infrastructure/webserver/server.js
+++ api/lib/infrastructure/webserver/server.js
@@ -1,13 +1,3 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
-const Good = require('@hapi/good');
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const Blipp = require('blipp');
-const HapiSwagger = require('hapi-swagger');
-const Package = require('../../../package');
-
 const createServer = async () => {
   require('dotenv').config();
 const express = require('express');
