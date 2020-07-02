879cc2256f2433ff31e59d71d169b9d69b5dab9d
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-03 00:05:46 +0200

    added finished elction array to the user schema

--- api/domain/User.js
+++ api/domain/User.js
@@ -1,9 +1,10 @@
 module.exports = class {
 
-  constructor(id = null, email, electionID ) {
+  constructor(id = null, email, electionID, finishedElectionIDs) {
     this.id = id;
     this.email = email;
     this.electionID = electionID;
+    this.finishedElectionIDs = finishedElectionIDs;
   }
 
 };
--- api/infrastructure/orm/mongoose/schemas/User.js
+++ api/infrastructure/orm/mongoose/schemas/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   //TODO: Change this to mongo ids
   electionID: String,
+  finishedElectionIDs: [String],
 });
 
 module.exports = mongoose.model('User', userSchema);
