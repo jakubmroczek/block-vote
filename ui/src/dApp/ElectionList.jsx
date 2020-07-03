6d47a7adb6a85da1c598b84d9b9bc00da6aa01d7
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-03 11:20:09 +0200

    displaying the election list for the public key

--- ui/src/dApp/ElectionList.jsx
+++ ui/src/dApp/ElectionList.jsx
@@ -1,7 +1,49 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import { Card, Table } from 'react-bootstrap';
+
+function ElectionRow({ election }) {
+  const { title } = election;
+  return (
+    <>
+      <tr>
+        <td>{title}</td>
+      </tr>
+    </>
+  );
+}
+
+function ElectionTable({ elections }) {
+  const rows = elections
+    .map((election, index) => (
+      <ElectionRow
+        index={index}
+        election={election}
+      />
+    ));
+
+  return (
+    <Table bordered condensed hover responsive className="text-left">
+      <tbody>
+        {rows}
+      </tbody>
+    </Table>
+  );
+}
 
 export default class ElectionList extends React.Component {
   render() {
-    return (<>Hello from election lsit</>);
+    const { elections } = this.props;
+
+    return (
+      <Card className="text-center mt-2 mr-3">
+        <Card.Header as="h5">Your elections:</Card.Header>
+        <Card.Body>
+          <ElectionTable
+            elections={elections}
+          />
+        </Card.Body>
+      </Card>
+    );
   }
 }
