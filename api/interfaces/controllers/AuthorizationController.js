efdb42ceab20144a6cef52ef56591ccffa1bbc27
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-05 20:15:36 +0200

    refactorized AuthorizationContreool

--- api/interfaces/controllers/AuthorizationController.js
+++ api/interfaces/controllers/AuthorizationController.js
@@ -1,95 +1,52 @@
-const Boom = require('@hapi/boom');
-const GetAccessToken = require('../../application/use_cases/GetAccessToken');
-const VerifyAccessToken = require('../../application/use_cases/VerifyAccessToken');
+const GetAccessToken = require('../../application/use_cases/GetAccessToken.js');
+const VerifyAccessToken = require('../../application/use_cases/VerifyAccessToken.js');
+const VerifyGoogleOAuth2Token = require('../../application/use_cases/VerifyGoogleOAuth2Token.js');
+const CreateUser = require('../../application/use_cases/CreateUser.js');
 
 module.exports = {
 
-  async getAccessToken_2(request, response) {
+  async getAccessToken(request, response) {
     const googleToken = request.body.google_token;
+    const { serviceLocator } = request.app.parent;
+    
     if (!googleToken) {
       response.status(400).send({ code: 400, message: 'Missing Token' });
       return;
     }
 
-    const client = new OAuth2Client();
-    let payload;
-    
     try {
-      const ticket = await client.verifyIdToken({ idToken: googleToken });
-      payload = ticket.getPayload();
+      const credentials = await VerifyGoogleOAuth2Token(googleToken, serviceLocator);
+      const token = GetAccessToken(credentials, serviceLocator);
+      
+      response.cookie('jwt', token, { httpOnly: true });
+
+      // TODO: Refactor this code, too many things does happen here
+      // We use the email as the username
+      //! !!! Email is the username
+      await registerIfNewUser(credentials, serviceLocator);
+
+      response.json(credentials);
     } catch (error) {
+      console.log(error);
       response.status(403).send('Invalid credentials');
     }
-
-    const { given_name: username, name, email } = payload;
-    const credentials = {
-      isLoggedIn: true, username, name, email,
-    };
-
-    const token = jwt.sign(credentials, JWT_SECRET);
-    res.cookie('jwt', token, { httpOnly: true });
-  
-    
-    // TODO: Refactor this code, too many things does happen here
-    // We use the email as the username
-    //! !!! Email is the username
-    const { app } = req;
-    const { parent } = app;
-    const { serviceLocator } = parent;
-    await registerIfNewUser(email, serviceLocator);
-  
-    res.json(credentials);
   },
 
-
-  async getAccessToken(request) {
-
-    // Context
-    const serviceLocator = request.server.app.serviceLocator;
-
-    // Input
-    const grantType = request.payload['grant_type'];
-    const email = request.payload['username'];
-    const password = request.payload['password'];
-
-    if (!grantType || grantType !== 'password') {
-      return Boom.badRequest('Invalid authentication strategy');
+  verifyAccessToken(request) {
+    const token = request.cookies.jwt;
+  
+    if (!token) {
+      return { isLoggedIn: false };
     }
-
-    // Treatment
+  
     try {
-      const accessToken = await GetAccessToken(email, password, serviceLocator);
-
-      // Output
-      return accessToken;
-    } catch (err) {
-      return Boom.unauthorized('Bad credentials');
-    }
-  },
-
-  verifyAccessToken(request, h) {
-
-    // Context
-    const serviceLocator = request.server.app.serviceLocator;
-
-    // Input
-    const authorizationHeader = request.headers.authorization;
-    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-      throw Boom.badRequest('Missing or wrong Authorization request header', 'oauth');
-    }
-    const accessToken = authorizationHeader.replace(/Bearer/gi, '').replace(/ /g, '');
-
-    // Treatment
-    try {
-      const { uid } = VerifyAccessToken(accessToken, serviceLocator);
-
-      // Output
-      return h.authenticated({
-        credentials: { uid },
-        artifacts: { accessToken: accessToken }
-      });
-    } catch (err) {
-      return Boom.unauthorized('Bad credentials');
+      const { serviceLocator } = request.app;
+      const credentials = VerifyAccessToken(token, serviceLocator);
+      return credentials;
+    } catch (error) {
+      // Maybe error is here? causing when client refreshes
+      console.log(error);
+      return { isLoggedIn: false };
     }
   },
 
