747c48a71382af7c6e8383f8d2212c11a6770e05
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-05 11:13:35 +0200

    changed the layout of the welcome page

--- ui/src/Welcome.jsx
+++ ui/src/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Button, Figure,
+  Button, Figure, Container, Row, Col,
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -20,13 +20,31 @@ function Logo() {
 // TODO: Rename to welcome page
 export default function Welcome() {
   return (
-    <>
-      <Logo />
-      <LinkContainer to="/vote">
-        <Button>
-          Vote
-        </Button>
-      </LinkContainer>
-    </>
+    <Container className="mt-3 text-center">
+      <Row>
+        <Col>
+          <Logo />
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{fontSize: 20}}>
+          Your truly decentralized voting
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <LinkContainer to="/vote">
+            <Button variant="outline-success">
+              Vote
+            </Button>
+          </LinkContainer>
+          <LinkContainer to="/vote">
+            <Button className="ml-1" variant="outline-info">
+              About
+            </Button>
+          </LinkContainer>
+        </Col>
+      </Row>
+    </Container>
   );
 }
