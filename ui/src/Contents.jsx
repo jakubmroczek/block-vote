3aeb5b27d2d3ee98eab1883f2df38c32766ab51c
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-05 11:51:06 +0200

    implemented routing logic when user is logged in

--- ui/src/Contents.jsx
+++ ui/src/Contents.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import ElectionSetUpPanel from './ElectionSetUpPanel.jsx';
 import UserPanel from './UserPanel.jsx';
@@ -8,22 +9,38 @@ import WelcomePage from './WelcomePage.jsx';
 import RegisterPublicKeyPanel from './register/RegisterPublicKeyPanel.jsx';
 import DApp from './dApp/DApp.jsx';
 import Election from './dApp/Election.jsx';
+import UserContext from './UserContext.js';
 
 const NotFound = () => <h1>Error: page not found</h1>;
 
-export default function Contents({ setUser }) {
-  return (
-    <>
-      <Route path="/" render={props => <NavBar {...props} setUser={setUser} />} />
-      <Switch>
-        <Route exact path="/" component={WelcomePage} />
-        <Route exact path="/panel" component={UserPanel} />
-        <Route path="/panel/edit/:electionID" component={ElectionSetUpPanel} />
-        <Route exact path="/vote" component={DApp} />
-        <Route exact path="/vote/:electionID" component={Election} />
-        <Route path="/register/:electionID" component={RegisterPublicKeyPanel} />
-        <Route component={NotFound} />
-      </Switch>
-    </>
-  );
+export default class Contents extends React.Component {
+  render() {
+    const user = this.context;
+    const { isLoggedIn } = user;
+    const { setUser } = this.props;
+    return (
+      <>
+        <Route path="/" render={props => <NavBar {...props} setUser={setUser} />} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (isLoggedIn
+              ? <Redirect to="/panel" />
+              : <WelcomePage />
+
+            )}
+          />
+          <Route exact path="/panel" component={UserPanel} />
+          <Route path="/panel/edit/:electionID" component={ElectionSetUpPanel} />
+          <Route exact path="/vote" component={DApp} />
+          <Route exact path="/vote/:electionID" component={Election} />
+          <Route path="/register/:electionID" component={RegisterPublicKeyPanel} />
+          <Route component={NotFound} />
+        </Switch>
+      </>
+    );
+  }
 }
+
+Contents.contextType = UserContext;
