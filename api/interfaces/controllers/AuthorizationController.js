f7bec4525192a0fd830e6b9936ab0bdb25c4a5c9
Jakub Mroczek <jakub.mroczek@tomtom.com>
2020-07-05 20:31:27 +0200

    removed creating user if absent

--- api/interfaces/controllers/AuthorizationController.js
+++ api/interfaces/controllers/AuthorizationController.js
@@ -1,14 +1,13 @@
 const GetAccessToken = require('../../application/use_cases/GetAccessToken.js');
 const VerifyAccessToken = require('../../application/use_cases/VerifyAccessToken.js');
 const VerifyGoogleOAuth2Token = require('../../application/use_cases/VerifyGoogleOAuth2Token.js');
-const CreateUser = require('../../application/use_cases/CreateUser.js');
 
 module.exports = {
 
   async getAccessToken(request, response) {
     const googleToken = request.body.google_token;
     const { serviceLocator } = request.app.parent;
-    
+
     if (!googleToken) {
       response.status(400).send({ code: 400, message: 'Missing Token' });
       return;
@@ -17,14 +16,7 @@ module.exports = {
     try {
       const credentials = await VerifyGoogleOAuth2Token(googleToken, serviceLocator);
       const token = GetAccessToken(credentials, serviceLocator);
-      
       response.cookie('jwt', token, { httpOnly: true });
-
-      // TODO: Refactor this code, too many things does happen here
-      // We use the email as the username
-      //! !!! Email is the username
-      await registerIfNewUser(credentials, serviceLocator);
-
       response.json(credentials);
     } catch (error) {
       console.log(error);
@@ -34,11 +26,11 @@ module.exports = {
 
   verifyAccessToken(request) {
     const token = request.cookies.jwt;
-  
+
     if (!token) {
       return { isLoggedIn: false };
     }
-  
+
     try {
       const { serviceLocator } = request.app;
       const credentials = VerifyAccessToken(token, serviceLocator);
